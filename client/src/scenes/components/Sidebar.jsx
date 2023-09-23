df8a31eab3323ff3c5a0d71e74455622640b4fb0
Tayyab Tariq <tayyabashraf22@gmail.com>
2023-09-23 23:59:35 +0500

    commit

--- client/src/scenes/components/Sidebar.jsx
+++ client/src/scenes/components/Sidebar.jsx
@@ -197,14 +197,7 @@ const Sidebar = ({
             </List>
           </Box>
 
-          <Box position="absolute" bottom="2rem">
-            <Divider />
-            <FlexBetween textTransform="none" gap="1rem">
-              <Box></Box>
-            </FlexBetween>
-          </Box>
-
-          <Box position="absolute" bottom="2rem">
+          <Box mb="1rem">
             <Divider />
             <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
               <Box
